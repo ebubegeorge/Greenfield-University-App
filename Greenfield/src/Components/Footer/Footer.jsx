20ea4114766c946b1b3dbfe29478b799c1850f8d
Ebube George <ebubegeorge64@gmail.com>
2026-04-21 10:08:48 +0100

    progress update

--- Greenfield/src/Components/Footer/Footer.jsx
+++ Greenfield/src/Components/Footer/Footer.jsx
@@ -1,10 +1,28 @@
-import './footer.css'
-
+import React from 'react';
+import './Footer.css';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <footer>
-            <p>&copy; 2023 My App. All rights reserved.</p>
+        <footer className='footer'>
+            <div className='footer-content'>
+                <div className='footer-brand'>
+                    <img src="faviconlogo.png" alt="logo icon" />
+                    <img src="GFUTEXTLOGO.png" alt="Greenfield" />
+                </div>
+            </div>
+            <ul className='footer-links'>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/About">About</Link></li>
+                <li><Link to="/Programs">Programs</Link></li>
+            </ul>
+            <div className='footer-contact'>
+                <p>Email: info@greenfield.edu</p>
+                <p>Phone: +1234567890</p>
+            </div>
+            <div className='footer-bottom'>
+            <p>&copy; 2026 Greenfield University. All rights reserved.</p>
+            </div>
         </footer>
     );
 }
