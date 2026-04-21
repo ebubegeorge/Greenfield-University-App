1ef1483c8f4c51ee9a79d294069943e3c61822fb
Ebube George <ebubegeorge64@gmail.com>
2026-04-21 11:09:08 +0100

    progress update

--- Greenfield/src/Pages/News/News.jsx
+++ Greenfield/src/Pages/News/News.jsx
@@ -1,8 +1,14 @@
+import React from 'react';
+import Navbar from '../../Components/Navbar/Navbar';
+import Footer from '../../Components/Footer/Footer';
+
 const News = () => {
     return (
         <div>
+            <Navbar />
             <h1>News</h1>
             <p>This is the news page of our application.</p>
+            <Footer />
         </div>
     );
 }
--- Greenfield/src/Pages/Programs/Programs.jsx
+++ Greenfield/src/Pages/Programs/Programs.jsx
@@ -1,8 +1,14 @@
+import React from 'react';
+import Navbar from '../../Components/Navbar/Navbar';
+import Footer from '../../Components/Footer/Footer';
+
 const Programs = () => {
     return (
         <div>
+            <Navbar />
             <h1>Programs</h1>
             <p>This is the programs page of our application.</p>
+            <Footer />
         </div>
     );
 }
