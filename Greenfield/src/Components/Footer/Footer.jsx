fdf9419776d4e243384297284763087901a3ff52
Ebube <ebubegeorge64@gmail.com>
2026-04-22 12:52:51 +0100

    update project structure

--- Greenfield/src/Components/Footer/Footer.jsx
+++ Greenfield/src/Components/Footer/Footer.jsx
@@ -8,20 +8,19 @@ const Footer = () => {
 
   <div className='footer-content'>
 
-    {/* LEFT */}
+
     <div className='footer-brand'>
       <img src="faviconlogo.png" alt="logo icon" />
       <img src="GFUTEXTLOGO.png" alt="Greenfield" />
     </div>
 
-    {/* CENTER */}
+
     <ul className='footer-links'>
       <li><Link to="/">Home</Link></li>
       <li><Link to="/about">About</Link></li>
       <li><Link to="/programs">Programs</Link></li>
     </ul>
 
-    {/* RIGHT */}
     <div className='footer-contact'>
       <p>Email: info@greenfield.edu</p>
       <p>Phone: +1234567890</p>
@@ -29,7 +28,6 @@ const Footer = () => {
 
   </div>
 
-  {/* BOTTOM */}
   <div className='footer-bottom'>
     <p>&copy; 2026 Greenfield University. All rights reserved.</p>
   </div>
