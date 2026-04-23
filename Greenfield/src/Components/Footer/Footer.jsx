dd74bf08407608b1829072161b9429d52b53c340
Ebube <ebubegeorge64@gmail.com>
2026-04-23 11:41:02 +0100

    update project structure

--- Greenfield/src/Components/Footer/Footer.jsx
+++ Greenfield/src/Components/Footer/Footer.jsx
@@ -3,37 +3,37 @@ import './Footer.css';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
-    return (
-<footer className='footer'>
+  return (
+    <footer className='footer'>
 
-  <div className='footer-content'>
-
-
-    <div className='footer-brand'>
-      <img src="faviconlogo.png" alt="logo icon" />
-      <img src="GFUTEXTLOGO.png" alt="Greenfield" />
-    </div>
+      <div className='footer-content'>
 
 
-    <ul className='footer-links'>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/about">About</Link></li>
-      <li><Link to="/programs">Programs</Link></li>
-    </ul>
+        <div className='footer-brand'>
+          <img src="faviconlogo.png" alt="logo icon" />
+          <img src="GFUTEXTLOGO.png" alt="Greenfield" />
+        </div>
 
-    <div className='footer-contact'>
-      <p>Email: info@greenfield.edu</p>
-      <p>Phone: +1234567890</p>
-    </div>
 
-  </div>
+        <ul className='footer-links'>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/programs">Programs</Link></li>
+        </ul>
 
-  <div className='footer-bottom'>
-    <p>&copy; 2026 Greenfield University. All rights reserved.</p>
-  </div>
+        <div className='footer-contact'>
+          <p>Email: info@greenfield.edu</p>
+          <p>Phone: +1234567890</p>
+        </div>
 
-</footer>
-    );
+      </div>
+
+      <div className='footer-bottom'>
+        <p>&copy; 2026 Greenfield University. All rights reserved.</p>
+      </div>
+
+    </footer>
+  );
 }
 
 export default Footer;
