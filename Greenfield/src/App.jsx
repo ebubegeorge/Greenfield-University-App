5f9a629fd318ad0f81798f0c7f7ca4c8c7a420ff
Ebube <ebubegeorge64@gmail.com>
2026-04-30 01:39:08 +0100

    update project structure

--- Greenfield/src/App.jsx
+++ Greenfield/src/App.jsx
@@ -1,23 +1,28 @@
 import './App.css'
-
-
+import { Routes, Route } from "react-router-dom";
 import About from './Pages/About/About'
 import Admmissions from './Pages/Admissions/Admissions'
 import Contact from './Pages/Contact/Contact'
 import Home from './Pages/Home/Home'
 import News from './Pages/News/News'
 import Programs from './Pages/Programs/Programs'
-import AppRoutes from './Routes/AppRoutes'
-import { Routes, Route } from "react-router-dom";
+import MainLayout from './Layout/MainLayout';
 
-function App() {
-
+const App = () => {
   return (
     <>
-      <AppRoutes />
-
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/admissions" element={<Admmissions />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/programs" element={<Programs />} />
+      </Route>
+    </Routes >
     </>
-  )
+  );
 }
 
-export default App
+export default App;
