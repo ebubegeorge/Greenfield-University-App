3660b6491e23723444f379fcb67c8231d44e95a5
Ebube <ebubegeorge64@gmail.com>
2026-05-07 08:09:55 +0100

    update project structure

--- Westbridge/src/Components/Navbar/Navbar.jsx
+++ Westbridge/src/Components/Navbar/Navbar.jsx
@@ -10,6 +10,7 @@ const Navbar = () => {
                 </div>
             </Link>
             <ul className='nav-links'>
+                <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/programs">Programs</Link></li>
                 <li><Link to="/admissions">Admissions</Link></li>
